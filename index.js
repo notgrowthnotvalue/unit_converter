530f0bc3bed911218988acfc70d64e871a98c01c
notgrowthnotvalue <96894573+notgrowthnotvalue@users.noreply.github.com>
2022-10-30 17:51:20 -0400

    Adding files to upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let convertBtn = document.getElementById("convert-btn");
+let inputEl = document.getElementById("input-el");
+let lengthEl = document.getElementById("length-el");
+let volumeEl = document.getElementById("volume-el");
+let massEl = document.getElementById("mass-el");
+
+const meterToFeet = 3.281;
+const literToGallon = 0.264;
+const kilogramToPound = 2.204;
+
+// On click, gets the value in the input and updates the string for each category
+convertBtn.addEventListener('click',function(){
+    let baseValue = inputEl.value;
+    lengthEl.textContent = `
+    ${baseValue} meters = ${(baseValue*meterToFeet).toFixed(3)} feet |
+    ${baseValue} feet = ${(baseValue/meterToFeet).toFixed(3)} meters
+    `
+    volumeEl.textContent = `
+    ${baseValue} liters = ${(baseValue*literToGallon).toFixed(3)} gallons |
+    ${baseValue} gallons = ${(baseValue/literToGallon).toFixed(3)} liters
+    `
+    massEl.textContent = `
+    ${baseValue} kilograms = ${(baseValue*kilogramToPound).toFixed(3)} pounds |
+    ${baseValue} pounds = ${(baseValue/kilogramToPound).toFixed(3)} kilograms
+    `        
+})
